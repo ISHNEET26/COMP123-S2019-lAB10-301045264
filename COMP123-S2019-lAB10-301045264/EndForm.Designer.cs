ef7c211d4b64e0c50a9156e44a9e4eb4fa08c967
ishneet26 <kaurishneet3106@gmail.com>
2019-07-19 15:23:27 -0400

    Lab 10 - Splash Screen - COMP 123

---
+++ COMP123-S2019-lAB10-301045264/EndForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace COMP123_S2019_lAB10_301045264
+{
+    partial class EndForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EndFormLabel = new System.Windows.Forms.Label();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EndFormLabel
+            // 
+            this.EndFormLabel.AutoSize = true;
+            this.EndFormLabel.Location = new System.Drawing.Point(211, 23);
+            this.EndFormLabel.Name = "EndFormLabel";
+            this.EndFormLabel.Size = new System.Drawing.Size(289, 31);
+            this.EndFormLabel.TabIndex = 0;
+            this.EndFormLabel.Text = "Welcome to End Form!";
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(45, 502);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(126, 47);
+            this.BackButton.TabIndex = 1;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // EndForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ControlBox = false;
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.EndFormLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.MaximizeBox = false;
+            this.Name = "EndForm";
+            this.Opacity = 0.8D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EndForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EndFormLabel;
+        private System.Windows.Forms.Button BackButton;
+    }
+}
---
+++ COMP123-S2019-lAB10-301045264/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_lAB10_301045264
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            Program.Forms[FormName.END_FORM].Show();
+            this.Hide();
+        }
+    }
+}
